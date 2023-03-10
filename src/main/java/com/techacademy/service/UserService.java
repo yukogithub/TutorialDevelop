a738db7b6ba207069de70b481c9fa8e3b4f637c7
yukogithub <netteoisii@gmail.com>
2023-03-10 19:48:39 +0900

    Lesson17 Chapter7

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -2,13 +2,15 @@ package com.techacademy.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    
+
     public UserService(UserRepository repository) {
         this.userRepository = repository;
     }
@@ -18,4 +20,12 @@ public class UserService {
         //　リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
     }
+
+    // ----- 追加：ここから -----
+    /** Userの登録を行う */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    // ----- 追加：ここまで-----
 }
